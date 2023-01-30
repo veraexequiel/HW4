07b3965df965ad5828e7c5aed0e090dac9c58986
veraexequiel <veraexequiel5@gmail.com>
2023-01-29 19:28:42 -0800

    added class and code for the add button

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
+let Exercise = function (pType, pMinutes, pCalories) {
+    this.type = pType;
+    this.minutes = pMinutes;
+    this.calories = pCalories;
+}
+
+let exercises = [];
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    document.getElementById("addBtn").addEventListener("click", function () {
+        let minutes = document.getElementById("minutesInput").value;
 
-        document.getElementById("textbox1").value = "success";
+        let radioButtonGroup = document.getElementsByName("exercise");
+        let checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
+        let exerciseType = checkedRadio.value;
+
+        let calories = 0;
+
+        if (exerciseType === "Sit Ups") {
+            calories = minutes * 10;
+        }
+
+        if (exerciseType === "Push Ups") {
+            calories = minutes * 15;
+        }
+
+        if (exerciseType === "Jump Rope") {
+            calories = minutes * 18;
+        }
+
+        document.getElementById("exerciseList").innerHTML = "<li>" + minutes + " minutes</li>";
+        let exercise = new Exercise(exerciseType, minutes, calories);
+        exercises.push(exercise);
+    });
+
+    document.getElementById("mostBurnedBtn").addEventListener("click", function () {
+
+        document.getElementById("mostBurnedInput").value = "clicked";
+    });
+
+    document.getElementById("showAllBtn").addEventListener("click", function () {
+
+        document.getElementById("exerciseList").innerHTML = "<li>Show all clicked</li>";
     });
 });
 
